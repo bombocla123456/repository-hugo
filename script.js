a547404a1f8048f191fd290bde6ab1493cd07a72
Hugo <hugovincent2010@gmail.com>
2026-06-18 16:57:56 +0200

    update de hugo

---
+++ script.js
@@ -0,0 +1,8 @@
+let nbLikes = 0;
+const boutonLike = document.getElementById("mon-bouton-like");
+if (boutonLike) {
+boutonLike.addEventListener("click", () -> {
+nbLikes = nbLikes + 1;
+boutonLike.textContent = "❤ J'aime (" + nbLikes + ")";
+});
+}
